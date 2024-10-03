809dc04703a27cd51b837bc499d3230de976c44f
Nat2244 <149074264+Nat2244@users.noreply.github.com>
2024-10-03 16:09:44 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+	require('db.php');
+	include 'header.php';
+	echo "<center><h3>Cat Cafe!</h3></center>";
+	echo '<center><div>New to this cat cafe? Please <a href="login.php">Login</a> or <a href="register.php">Register</a> </div></center>';
+	include 'footer.php';
