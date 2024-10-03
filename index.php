23656280d172afc90d59886849ee2e3c9ea4a1fc
Nat2244 <149074264+Nat2244@users.noreply.github.com>
2024-10-03 16:14:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 	require('db.php');
 	include 'header.php';
 	echo "<center><h3>Cat Cafe!</h3></center>";
-	echo '<center><div>New to this cat cafe? Please <a href="login.php">Login</a> or <a href="register.php">Register</a> </div></center>';
+	echo '<center><div>New to this cat cafe? Please go <a href="login.php">Login</a> or <a href="register.php">Register</a> </div></center>';
 	include 'footer.php';
