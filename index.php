dadc5b38e4f7db874768e9b043ec5df54cfe52c5
Nat2244 <149074264+Nat2244@users.noreply.github.com>
2024-10-09 12:00:10 +0100

    new comment

--- index.php
+++ index.php
@@ -4,3 +4,4 @@
 	echo "<center><h3>Cat Cafe!</h3></center>";
 	echo '<center><div>New to this cat cafe? Please go <a href="login.php">Login</a> or <a href="register.php">Register</a> </div></center>';
 	include 'footer.php';
+	//yayyy
