5db053dfceafb44d8cafd9e526e62a53c0d8254c
Nat2244 <149074264+Nat2244@users.noreply.github.com>
2024-10-16 09:23:17 +0100

    linked it with my new database

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "root";
-	$dbname = "cat_cafe";
+	$dbname = "cat_cafe_db";
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 	// Check connection
