5977332420c1375d0c40d3e307c47f4c84b87080
Nat2244 <149074264+Nat2244@users.noreply.github.com>
2024-11-14 16:02:33 +0000

    changed users to user, (working)

--- register.php
+++ register.php
@@ -6,7 +6,7 @@
 		$email = $_POST['email'];
 			$password = $_POST['password'];
 	 
-	 $sql = "INSERT INTO users (username, password, email)
+	 $sql = "INSERT INTO user (username, password, email)
 	VALUES ('$username', '$password', '$email')";
 
 	if (mysqli_query($conn, $sql)) {
